2fc04ed09be79dda6d4a5695a3268cc84e0eaafb
Matt Wagg <matt@mattwagg.com>
2020-01-04 13:35:18 +0000

    styled container for layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -26,20 +26,11 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <div className="Layout">
+    <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-      </div>
+      <main className="Layout">{children}</main>
       <Footer />
-    </div>
+    </>
   )
 }
 
