3e2ca6ab66f7c6a3fc4f99860577d89880febe74
Matt Wagg <matt@mattwagg.com>
2020-01-04 13:36:21 +0000

    rearranging markup for layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Matt Wagg<br />Frontend Developer</h1>
+    <p>An inspirational sentence about how I can make awesome things for you</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
