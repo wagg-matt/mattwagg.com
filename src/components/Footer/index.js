51e9cd6ffad08152845cc175394ac4c1a9f4f086
Matt Wagg <matt@mattwagg.com>
2020-01-04 13:28:38 +0000

    styled footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import "./index.css";
+
 const Footer = () => (
-  <footer>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
+  <footer className="footer">
+    <div className="footer__inner">
+      © {new Date().getFullYear()}, Built with
+      {` `}
+      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    </div>
   </footer>
 )
 
